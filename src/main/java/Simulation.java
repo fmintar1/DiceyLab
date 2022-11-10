341c0683ac382ecccd1ff581574e7dd6ec7791d1
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-09 23:42:35 -0500

    MY FINAL FORM

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 public class Simulation {
-
-
     public static void main(String[] args) {
         Dice dice = new Dice(2);
         Bins bins = new Bins(2, 12);
-        bins.binCount(1000000);
-        System.out.println("\n Dice  |    Rolls  | Percent  |  Level \n");
+        int numberOfRolls = 2147483647;
+        bins.binCount(numberOfRolls);
+        System.out.println("\n Dice  |    Rolls     | Percent  |  Level \n");
         bins.count.forEach((k,v) ->
-                System.out.printf("  %2d   |   %6d  |  %5.2f%%  |  %s\n", k, v, bins.getAverage(k), bins.starPrint(k)));
-        System.out.println("\n Total |  1000000  | 100.00%  |  *");
+                System.out.printf("  %2d   |   %9d  |  %5.2f%%  |  %s\n", k, v, bins.getAverage(k), bins.starPrint(k)));
+        System.out.println("\n Total |  " + numberOfRolls + "  | 100.00%  |  *");
         }
     }
 
